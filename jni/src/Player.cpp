47eb0d2312ff04033bba978ac581135305894de2
Chuck Valenza <fallencx@gmail.com>
2016-09-13 11:29:59 -0400

    Replace look with attack animation

--- jni/src/Player.cpp
+++ jni/src/Player.cpp
@@ -10,6 +10,8 @@
 
 void Player::_init()
 {
+	atk_anim_timer = std::chrono::milliseconds(0);
+
 	view->setPosition(game->getSize() / 2);
 	character = new Sprite;
 	character->setResAnim(res::ui.getResAnim("player"));
@@ -22,16 +24,19 @@ void Player::_init()
 	weapon->setVisible(false);
 }
 
-void Player::_update(const UpdateState& us)
+void Player::move(timeMS dt)
 {
-	weapon->setVisible(false);
-
 	Vector2 dir;
+	Vector2 pos = view->getPosition();
 
 	if (game->move->getDirection(dir)) {
-		Vector2 pos = view->getPosition();
-		pos = pos + dir * (us.dt / 1000.0f) * 5;
-		view->setPosition(pos);
+		Vector2 l_dir;
+
+		if (game->look->getDirection(l_dir)) {
+			pos = pos + dir * (dt / 1000.0f) * 3.5f;
+		} else {
+			pos = pos + dir * (dt / 1000.0f) * 5;
+		}
 
 		float angle = atan2f(dir.y, dir.x);
 
@@ -43,6 +48,48 @@ void Player::_update(const UpdateState& us)
 
 		view->setRotation(angle);
 	}
+
+	if (pos.x < 0.25f * game->getWidth()) {
+		pos.x = 0.25f * game->getWidth();
+	} else if (pos.x > 0.75f * game->getWidth()) {
+		pos.x = 0.75f * game->getWidth();
+	}
+
+	if (pos.y < 0.25f * game->getHeight()) {
+		pos.y = 0.25f * game->getHeight();
+	} else if (pos.y > 0.75f *  game->getHeight()) {
+		pos.y = 0.75f * game->getHeight();
+	}
+
+	view->setPosition(pos);
+}
+
+void Player::changeAnim(std::string action, int ms)
+{
+	if (action == "stand") {
+		character->addTween(TweenAnim(res::ui.getResAnim("player"), 0, 0), ms);
+	} else if (action, "swing") {
+		character->addTween(TweenAnim(res::ui.getResAnim("player"), 1, 4), ms);
+	}
+}
+
+void Player::swingAnimation(int ms)
+{
+	using namespace std::chrono;
+	milliseconds cur_time = duration_cast< milliseconds >
+		(system_clock::now().time_since_epoch());
+
+	if (cur_time - atk_anim_timer > milliseconds(ms)) {
+		character->setResAnim(res::ui.getResAnim("swing"));
+		changeAnim("swing", ms);
+		//character->addTween(TweenAnim(res::ui.getResAnim("player"), 1, 4), ms);
+		atk_anim_timer = cur_time;
+	}
+}
+
+void Player::attack()
+{
+	Vector2 dir;
 
 	if (game->look->getDirection(dir)) {
 		float angle = atan2f(dir.y, dir.x);
@@ -54,5 +101,18 @@ void Player::_update(const UpdateState& us)
 		}
 
 		view->setRotation(angle);
+
+		swingAnimation(300);
+	} else {
+		changeAnim("stand", 1);
+		//character->addTween(TweenAnim(res::ui.getResAnim("player"), 0, 0), 1);
 	}
+}
+
+void Player::_update(const UpdateState& us)
+{
+	weapon->setVisible(false);
+
+	move(us.dt);
+	attack();
 }
--- jni/src/Player.h
+++ jni/src/Player.h
@@ -6,18 +6,24 @@
 #define OXYGINE_HW_PLAYER_H
 
 #include "Unit.h"
+#include <chrono>
 
 DECLARE_SMART(Player, spPlayer);
 
 class Player : public Unit {
 	private:
-	public:
+		std::chrono::milliseconds atk_anim_timer;
+		void move(timeMS dt);
+		void attack();
+		void swingAnimation(int time_ms);
+		void changeAnim(std::string action, int ms);
 	protected:
 		void _init();
 		void _update(const UpdateState& us);
 
 		spSprite character;
 		spSprite weapon;
+	public:
 };
 
 #endif //OXYGINE_HW_PLAYER_H
